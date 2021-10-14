467075efdf6e18f37c45bc9d5cddd9249666bdd9
DCoxshall <63456774+DCoxshall@users.noreply.github.com>
2021-10-14 21:39:03 +0100

    Completed vectors in python

--- Vectors/Vector.py
+++ Vectors/Vector.py
@@ -10,6 +10,34 @@ class Vector:
         else:
             self.dimensions = [i for i in argv]
 
+    def __eq__(self, other):
+        if len(self.dimensions) != len(other.dimensions):
+            raise(Exception("Vectors are not of the same order: cannot test equality"))
+        for i in range(len(self.dimensions)):
+            if self.dimensions[i] != other.dimensions[i]:
+                return False
+        return True
+
+    def __lt__(self, other):
+        if self.magnitude() < other.magnitude():
+            return True
+        return False
+
+    def __le__(self, other):
+        if self.magnitude() <= other.magnitude():
+            return True
+        return False
+
+    def __gt__(self, other):
+        if self.magnitude() > other.magnitude():
+            return True
+        return False
+
+    def __ge__(self, other):
+        if self.magnitude() >= other.magnitude():
+            return True
+        return False
+
     def __add__(self, other):
         newDimensions = []
         selfDims = self.dimensions
@@ -21,6 +49,41 @@ class Vector:
         newDimensions += selfDims + otherDims
         return Vector(newDimensions)
 
+    def __sub__(self, other):
+        newDimensions = []
+        selfDims = self.dimensions
+        otherDims = other.dimensions
+        while (selfDims != [] and otherDims != []):
+            newDimensions.append(selfDims[0] - otherDims[0])
+            selfDims = selfDims[1:]
+            otherDims = otherDims[1:]
+        newDimensions += selfDims + otherDims
+        return Vector(newDimensions)
+
+    def magnitude(self):
+        return sqrt(sum([i ** 2 for i in self.dimensions]))
+
+    def dot(self, other):
+        if len(self.dimensions) != len(other.dimensions):
+            raise(
+                Exception("Vectors are not of the same order: dot product is undefined"))
+        return(sum([self.dimensions[i] * other.dimensions[i] for i in range(len(self.dimensions))]))
+
+    def cross(self, other):
+        if len(self.dimensions) != len(other.dimensions):
+            raise(
+                Exception("Vectors are not of the same order, cross product is undefined"))
+        if len(self.dimensions) > 3:
+            raise(Exception("Cross product is not defined for vectors of order > 3."))
+        newDims = []
+        newDims.append(
+            self.dimensions[1] * other.dimensions[2] - self.dimensions[2] * other.dimensions[1])
+        newDims.append(
+            self.dimensions[2] * other.dimensions[0] - self.dimensions[0] * other.dimensions[2])
+        newDims.append(
+            self.dimensions[0] * other.dimensions[1] - self.dimensions[1] * other.dimensions[0])
+        return Vector(newDims)
+
     def __str__(self):
         vecString = 'Vector<'
         for i in self.dimensions:
@@ -28,8 +91,3 @@ class Vector:
         vecString = vecString[:-2]
         vecString += '>'
         return vecString
-
-
-v1 = Vector(1, 2, 3)
-v2 = Vector(4, 5, 6)
-print(v1 + v2)
--- Vectors/Vector3d.py
+++ Vectors/Vector3d.py
@@ -1,6 +1,7 @@
 from math import sqrt
 
-#Demonstration of operator overriding in Python. This is a simple 3d Vector class
+# 3-Dimensional vector class, demonstrating operator overloading
+
 
 class Vector:
     def __init__(self, *args):
@@ -26,7 +27,7 @@ class Vector:
         return f"Vector<{self.x}, {self.y}, {self.z}>"
 
     def dot(self, other):
-        return sum([self.x  * other.x, self.y * other.y, self.z * other.z])
+        return sum([self.x * other.x, self.y * other.y, self.z * other.z])
 
     def __eq__(self, other):
         if self.x == other.x and self.y == other.y and self.z == other.z:
@@ -44,9 +45,3 @@ class Vector:
 
     def __ge__(self, other):
         return self.mag() >= other.mag()
-
-if __name__ == "__main__":
-    v1 = Vector(1,2,3)
-    v2 = Vector(4,5,6)
-
-    print(v1 < v2)
