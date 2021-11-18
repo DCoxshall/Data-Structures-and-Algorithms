ee7d3c3a14987f3bbfd87a58fcfbfeb290a00189
DCoxshall <63456774+DCoxshall@users.noreply.github.com>
2021-11-18 14:19:11 +0000

    Update graphgeneration.py

--- graphgeneration.py
+++ graphgeneration.py
@@ -5,21 +5,18 @@
 # {a: {b: z, c: y}} where a, b, and c are elements and z and y are one-way
 # connections to b and c from a. 
 
-import random
-
-def generateGraph(n) -> dict:
+def generateGraph(n, bi_directional = True) -> dict:
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     graph = {alphabet[i]: {} for i in range(n)}
-    nodes = list(graph.keys())
-    #for i in graph:
-    print(nodes)
+
+    graph = connect_nodes(graph, "C", "D", 10, bi_directional)
+    
     print(graph)
 
-    for i in graph:
-        temp_nodes = nodes
-        for i in range(random.randint(0, 4)):
-            node = random.choice(temp_nodes)
-            
-        
-        
-generateGraph(5)
+def connect_nodes(graph, a, b, weight, bi_directional = True):
+    graph[a][b] = weight
+    if bi_directional:
+        graph[b][a] = weight
+    return graph
+    
+generateGraph(5, True)
