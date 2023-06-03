09ab84e30b9bb350d16137670fcdaa58cb526371
Dylan Coxshall <dacoxshall@gmail.com>
2023-06-03 22:32:15 +0100

    fixed queue.py

--- Queues/queue.py
+++ Queues/queue.py
@@ -1,3 +1,15 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def push(self, data):
+        if self.next == None:
+            self.next = Node(data)
+        else:
+            self.next.push(data)
+
+
 class Queue:
     def __init__(self):
          self.head = None
@@ -11,21 +23,9 @@ class Queue:
         self.size += 1
 
     def dequeue(self):
-        if self.size == 0:
-            return None
-        else:
+        data = None
+        if self.size > 0:
             data = self.head.data
             self.head = self.head.next
-            return data
-        size -= 1
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def push(self, data):
-        if self.next == None:
-            self.next = Node(data)
-        else:
-            self.next.push(data)
+            size -= 1
+        return data
